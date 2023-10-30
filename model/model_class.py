eabd9080a6b179ea5a6485f064ff8a8c41120dcc
Kairo Silveira <kairo.silveira.contato@gmail.com>
2023-10-30 20:19:10 -0300

    model_class code implemented

---
+++ model/model_class.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torchvision.models as models
+
+
+class ElectronicClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.base_model = models.resnet18(pretrained=True)
+        self.base_model.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        x = self.base_model(x)
+        x = nn.functional.softmax(x,1)
+        return x
