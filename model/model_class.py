14bdf99df61489888959934f7fec5a327ff984d3
Kairo Silveira <kairo.silveira.contato@gmail.com>
2023-10-30 21:50:12 -0300

    updating the name of directories to generalize and to track raw data

--- model/model_class.py
+++ model/model_class.py
@@ -9,6 +9,4 @@ class ElectronicClassifier(nn.Module):
         self.base_model.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        x = self.base_model(x)
-        x = nn.functional.softmax(x,1)
-        return x
+        return self.base_model(x)
