e1bf177286360eb256d95a9e6689ed195e662133
Kairo Silveira <kairo.silveira.contato@gmail.com>
2023-11-06 19:58:25 -0300

    change split data to allow the creation of a validation set

--- data/split_data.py
+++ data/split_data.py
@@ -2,7 +2,6 @@ import os
 import shutil
 import random
 
-
 def create_directory(directory, overwrite=True):
     """
     Create a directory, optionally overwriting it if it exists.
@@ -24,17 +23,19 @@ def create_directory(directory, overwrite=True):
 
     os.makedirs(directory)
 
-
-# Split data into train and test
-def split_data_into_train_test(data_dir, train_dir, test_dir, split_ratio=0.8, random_state=42, overwrite_dirs=True):
+# Split data into train, validation, and test
+def split_data_into_train_val_test(data_dir, train_dir, val_dir, test_dir, 
+                                  split_ratio=(0.6, 0.2, 0.2), random_state=42, overwrite_dirs=True):
     """
-    Split data into train and test sets.
+    Split data into train, validation, and test sets.
 
     Args:
         data_dir (str): Path to the data directory.
         train_dir (str): Path to the training data directory.
+        val_dir (str): Path to the validation data directory.
         test_dir (str): Path to the testing data directory.
-        split_ratio (float, optional): The ratio of data to be in the training set. Defaults to 0.8.
+        split_ratio (tuple, optional): A tuple specifying the ratios for train, validation, and test sets. 
+                                     Defaults to (0.6, 0.2, 0.2).
         random_state (int, optional): Random seed for reproducibility. Defaults to 42.
         overwrite_dirs (bool, optional): Whether to overwrite existing directories. Defaults to True.
 
@@ -43,8 +44,9 @@ def split_data_into_train_test(data_dir, train_dir, test_dir, split_ratio=0.8, r
     """
     random.seed(random_state)
 
-    # Create or overwrite train and test directories
+    # Create or overwrite train, validation, and test directories
     create_directory(train_dir, overwrite=overwrite_dirs)
+    create_directory(val_dir, overwrite=overwrite_dirs)
     create_directory(test_dir, overwrite=overwrite_dirs)
 
     # Split data for each class
@@ -52,28 +54,35 @@ def split_data_into_train_test(data_dir, train_dir, test_dir, split_ratio=0.8, r
         class_path = os.path.join(data_dir, class_folder)
         if os.path.isdir(class_path):
             train_class_path = os.path.join(train_dir, class_folder)
+            val_class_path = os.path.join(val_dir, class_folder)
             test_class_path = os.path.join(test_dir, class_folder)
             os.makedirs(train_class_path, exist_ok=True)
+            os.makedirs(val_class_path, exist_ok=True)
             os.makedirs(test_class_path, exist_ok=True)
 
             class_images = os.listdir(class_path)
             num_images = len(class_images)
-            num_train = int(num_images * split_ratio)
+            num_train = int(num_images * split_ratio[0])
+            num_val = int(num_images * split_ratio[1])
+
             train_images = random.sample(class_images, num_train)
+            remaining_images = list(set(class_images) - set(train_images))
+            val_images = random.sample(remaining_images, num_val)
 
             for image in class_images:
                 source_path = os.path.join(class_path, image)
                 if image in train_images:
                     destination_path = os.path.join(train_class_path, image)
+                elif image in val_images:
+                    destination_path = os.path.join(val_class_path, image)
                 else:
                     destination_path = os.path.join(test_class_path, image)
                 shutil.copy(source_path, destination_path)
 
-
 if __name__ == "__main__":
-    data_dir = os.path.join("data/data_resistors", 'resistors')
-    train_dir = os.path.join("data/data_resistors", 'train')
-    test_dir = os.path.join("data/data_resistors", 'test')
+    data_dir = os.path.join("data/data_electronic", 'raw')
+    train_dir = os.path.join("data/data_electronic", 'train')
+    val_dir  = os.path.join("data/data_electronic", "validation")
+    test_dir = os.path.join("data/data_electronic", 'test')
 
-    # Create or overwrite train and test directories
-    split_data_into_train_test(data_dir, train_dir, test_dir, overwrite_dirs=True)
+    split_data_into_train_val_test(data_dir, train_dir, val_dir, test_dir)
