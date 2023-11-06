0691235fca90cbdf34ef067b1051b15869ada1d3
Kairo Silveira <kairo.silveira.contato@gmail.com>
2023-11-06 19:59:11 -0300

    fix the name of the model class

--- model/model_class.py
+++ model/model_class.py
@@ -3,7 +3,7 @@ from torchvision.models.resnet import ResNet18_Weights
 from torchvision.models import resnet18
 
 
-class ElectronicClassifier(nn.Module):
+class ElectronicsClassifier(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.base_model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
@@ -12,4 +12,3 @@ class ElectronicClassifier(nn.Module):
 
     def forward(self, x):
         return self.base_model(x)
-    
