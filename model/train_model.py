60bcbb2d0a9b2e04074f9553ae3a33c51fd5824d
Kairo Silveira <kairo.silveira.contato@gmail.com>
2023-11-06 21:04:36 -0300

    fixing return and add string doc of train function

--- model/train_model.py
+++ model/train_model.py
@@ -7,22 +7,42 @@ import torchvision.datasets as datasets
 from data.preprocess_data import AutoOrient, MakeSquare, ReduceImage
 from model.model_class import ElectronicsClassifier
 
-def train_model(data_dir, train_dir, test_dir, num_epochs, batch_size, lr, scale_factor, loss_function, optimizer):
-    # Define data transforms including the variable scale factor
-    transform = transforms.Compose([
-        AutoOrient(),  # Automatically orient the image
-        MakeSquare(),  # Center crop to make it square
-        ReduceImage(scale_factor),  # Variable scale factor
-        transforms.Resize((224, 224)),  # Resize to a standard size
-        transforms.ToTensor(),  # Convert to a tensor
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+def train_model(train_dataset, test_dataset, num_epochs, batch_size, lr, loss_function, optimizer, device):  
+    """
+    Train a neural network model on a given training dataset and evaluate it on a test dataset.
 
-    # Create datasets
-    train_dataset = datasets.ImageFolder(train_dir, transform=transform)
-    test_dataset = datasets.ImageFolder(test_dir, transform=transform)
+    Args:
+    train_dataset (torch.utils.data.Dataset): The training dataset.
+    test_dataset (torch.utils.data.Dataset): The test dataset.
+    num_epochs (int): The number of training epochs.
+    batch_size (int): The batch size for training.
+    lr (float): The learning rate for optimization.
+    loss_function (torch.nn.Module): The loss function to use.
+    optimizer (torch.optim.Optimizer): The optimizer for model weights adjustment.
+    device (torch.device): The device (e.g., 'cpu' or 'cuda') on which to perform the training.
 
-    # Create data loaders
+    Returns:
+    float: The test accuracy achieved by the model after training.
+
+    The function trains a neural network model on the provided training dataset for the specified number of epochs
+    using the specified hyperparameters. After training, it evaluates the model's performance on the test dataset
+    and returns the test accuracy.
+
+    Example:
+    >>> train_dataset = CustomDataset(train_data, train_labels)
+    >>> test_dataset = CustomDataset(test_data, test_labels)
+    >>> num_epochs = 10
+    >>> batch_size = 32
+    >>> lr = 0.001
+    >>> loss_function = nn.CrossEntropyLoss()
+    >>> optimizer = optim.Adam
+    >>> device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    >>> test_accuracy = train_model(train_dataset, test_dataset, num_epochs, batch_size, lr, loss_function, optimizer, device)
+    >>> print(f"Test Accuracy: {test_accuracy:.2f}")
+
+    """
+    
+    
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
@@ -35,7 +55,6 @@ def train_model(data_dir, train_dir, test_dir, num_epochs, batch_size, lr, scale
     optimizer = optimizer(model.parameters(), lr=lr)
 
     # Training loop
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
 
     for epoch in range(num_epochs):
@@ -53,9 +72,7 @@ def train_model(data_dir, train_dir, test_dir, num_epochs, batch_size, lr, scale
 
             running_loss += loss.item()
 
-        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {running_loss / len(train_loader)}")
-
-    # Evaluation
+    # Evaluation on the test dataset
     model.eval()
     correct = 0
     total = 0
@@ -69,17 +86,5 @@ def train_model(data_dir, train_dir, test_dir, num_epochs, batch_size, lr, scale
             correct += (predicted == labels).sum().item()
 
     accuracy = correct / total
-    print(f"Test Accuracy: {accuracy:.2f}")
 
-# Example usage:
-data_dir = 'Resistores'
-train_dir = 'train'
-test_dir = 'test'
-num_epochs = 10
-batch_size = 16
-lr = 0.001
-scale_factor = 0.50  # You can adjust this value
-loss_function = nn.CrossEntropyLoss()
-optimizer = optim.Adam
-
-train_model(data_dir, train_dir, test_dir, num_epochs, batch_size, lr, scale_factor, loss_function, optimizer)
+    return accuracy
